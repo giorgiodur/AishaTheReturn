40dcad1ca19283ee16594cee62a2e481fb899d63
Giorgio <gorgo.durante@gmail.com>
2026-02-05 15:33:46 +0100

    1 Step: inizio

---
+++ AishaTheReturnProject/AishaTheReturnProject/AishaTheReturnProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AishaTheReturnProjectApp.swift
+//  AishaTheReturnProject
+//
+//  Created by Giorgio Durante on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AishaTheReturnProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
